208783559ca161c0f58ba8e6e7736cab5776b167
andy8421 <yichang8421@qq.com>
2021-07-27 21:52:43 +0800

    快慢指针找中点+部分链表反转+逐个节点比较+不改变原链表仅判断

---
+++ 234. Palindrome Linked List/234. Palindrome Linked List.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ let reverseLN = function(head){
+    let pre =null, curr = head; next = null;
+    while(curr){
+        next = curr.next;
+        curr.next = pre;
+        pre = curr;
+        curr = next;
+    }
+    return pre;
+}
+
+let isPalindrome = function(head) {
+    let fast = slow = head;
+    let left = head,right = null;
+    let p = head,q = null;      // p、q用于还原链表
+
+    while(fast && fast.next){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    if(fast){
+        slow = slow.next;
+    }
+
+    right = reverseLN(slow);
+   
+    q = right;
+    
+    if(p.next!=slow){
+        p = p.next;
+    }
+
+    while(right){
+        if(left.val !== right.val){
+             return false;
+        }
+
+        left = left.next;
+        right = right.next;
+    }
+
+    // 还原链表
+    p.next = reverseLN(q);
+
+    return true;
+};
---
+++ 234. Palindrome Linked List/234. Palindrome Linked List.ts
@@ -0,0 +1,56 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+function reverseLN(head: ListNode | null): ListNode {
+    let pre = null, curr = head, next = null;
+    while (curr) {
+        next = curr.next;
+        curr.next = pre;
+        pre = curr;
+        curr = next;
+    }
+    return pre!;
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+    let fast = head, slow = head;
+    let left = head, right = null;
+    let p = head, q = null;      // p、q用于还原链表
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow!.next;
+    }
+    if (fast) {
+        slow = slow!.next;
+    }
+
+    right = reverseLN(slow);
+
+    q = right;
+
+    if (p && p.next != slow) {
+        p = p.next;
+    }
+
+    while (right) {
+        if (left!.val !== right.val) {
+            return false;
+        }
+
+        left = left!.next;
+        right = right.next;
+    }
+
+    // 还原链表
+    p!.next = reverseLN(q);
+
+    return true;
+};
