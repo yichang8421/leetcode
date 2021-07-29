c7c7173f0d44fc6b3a71bfca3e12d2b3fab399cf
andy8421 <yichang8421@qq.com>
2021-07-29 19:02:22 +0800

    二分法

---
+++ Array/35. Search Insert Position/35. Search-Insert-Position.ts
@@ -0,0 +1,14 @@
+function searchInsert(nums: number[], target: number): number {
+    let low:number = 0, high:number = nums.length-1;
+    while(low <= high){
+        let mid:number = low+((high-low)>>1);
+        if(nums[mid]>target){
+            high = mid-1;
+        }else if(nums[mid]<target){
+            low = mid+1;
+        }else{
+            return mid;
+        }
+    }
+    return low;
+};
